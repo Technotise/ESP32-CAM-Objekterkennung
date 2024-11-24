1c844aa7e3711c5aac513c99866e7850bc863308
Technotise <87918682+Technotise@users.noreply.github.com>
2024-11-24 02:49:19 +0100

    Create wifi_credentials.h

---
+++ ESP32/main/wifi_credentials.h
@@ -0,0 +1,8 @@
+// wifi_credentials.h
+#ifndef WIFI_CREDENTIALS_H
+#define WIFI_CREDENTIALS_H
+
+const char* WIFI_SSID = "XXXX";
+const char* WIFI_PASSWORD = "XXXX";
+
+#endif  // WIFI_CREDENTIALS_H
