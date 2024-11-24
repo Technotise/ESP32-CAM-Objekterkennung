4f4087c6067e0f87cc0d5c8cb68c153324ea53e3
Technotise <87918682+Technotise@users.noreply.github.com>
2024-11-24 02:43:32 +0100

    Create sd_card_mount.h

---
+++ ESP32/main/sd_card_mount.h
@@ -0,0 +1,8 @@
+// sd_card_mount.h
+#ifndef SD_CARD_MOUNT_H
+#define SD_CARD_MOUNT_H
+
+void mount_sd_card(const char* mount_point);
+void list_files(const char *mount_point);
+
+#endif // SD_CARD_MOUNT_H
